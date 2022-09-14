def45fe66f459e64e46ea38136f4b80b6b1c040a
lefmarna <panume5ya@gmail.com>
2022-09-14 22:08:14 +0900

    feat: 個体値のボタンを追加

--- src/components/organisms/IndividualValueField.tsx
+++ src/components/organisms/IndividualValueField.tsx
@@ -1,8 +1,9 @@
-import { Grid, TextField } from '@mui/material'
-import { ChangeEvent, useRef } from 'react'
+import { Box, Grid, TextField } from '@mui/material'
+import { ChangeEvent, MouseEvent, useRef } from 'react'
 import { Stat } from '../../types'
 import { MAX_IV } from '../../utils/constants'
 import { convertToInteger } from '../../utils/utilities'
+import { CalcButton } from '../molecules/CalcButton'
 
 type Props = {
   stats: Stat[]
@@ -25,8 +26,13 @@ export const IndividualValueField = (props: Props) => {
     updateIndividualValue(formatValue, statsIndex)
   }
 
+  const onClickCalcButton = (event: MouseEvent<HTMLElement>, effortValue: number) => {
+    const formatValue = effortValue !== 0 ? effortValue : ''
+    updateIndividualValue(formatValue, statsIndex)
+  }
+
   return (
-    <Grid item xs={5} sx={{ pl: { xs: 2, md: 4 } }}>
+    <Grid item xs={5} sx={{ pl: { xs: 2, md: 4 }, display: 'flex' }}>
       <TextField
         type="tel"
         label="個体値"
@@ -40,6 +46,10 @@ export const IndividualValueField = (props: Props) => {
           shrink: true,
         }}
       />
+      <Box>
+        <CalcButton onClick={onClickCalcButton}>{31}</CalcButton>
+        <CalcButton onClick={onClickCalcButton}>{0}</CalcButton>
+      </Box>
     </Grid>
   )
 }
