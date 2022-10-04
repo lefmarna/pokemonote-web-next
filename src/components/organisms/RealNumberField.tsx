aea96230f021aad42e606108f6bdfe9ba92e7b44
lefmarna <panume5ya@gmail.com>
2022-10-04 21:51:09 +0900

    feat: 実数値を自由入力できるよう実装

--- src/components/organisms/RealNumberField.tsx
+++ src/components/organisms/RealNumberField.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Grid, TextField } from '@mui/material'
-import { FocusEvent, useRef } from 'react'
+import { ChangeEvent, FocusEvent, useRef } from 'react'
 import { Nature, PokemonData, Stat } from '../../types'
 import {
   HP_INDEX,
@@ -95,6 +95,11 @@ export const RealNumberField = (props: Props) => {
     updateEffortValue(verifiedSetEffortValue, statsIndex)
   }
 
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const formatValue = convertToInteger(event.target.value, 999)
+    updateRealNumber(formatValue, statsIndex)
+  }
+
   const incrementRealNumber = () => {
     updateRealNumber(realNumber + 1, statsIndex)
   }
@@ -103,59 +108,59 @@ export const RealNumberField = (props: Props) => {
     updateRealNumber(realNumber - 1, statsIndex)
   }
 
-  const updateRealNumber = (realNumber: number) => {
-    let setValue = Number(convertToInteger(realNumber, MAX_REAL_NUMBER, false))
-    const formatLv = numberToInt(level, MIN_LEVEL)
-    const formatIndividualValue = numberToInt(stats[statsIndex].individualValue)
+  // const updateRealNumber = (realNumber: number) => {
+  //   let setValue = Number(convertToInteger(realNumber, MAX_REAL_NUMBER, false))
+  //   const formatLv = numberToInt(level, MIN_LEVEL)
+  //   const formatIndividualValue = numberToInt(stats[statsIndex].individualValue)
 
-    // HPのみ計算式が異なる
-    if (statsIndex === HP_INDEX) {
-      setValue =
-        (Math.ceil(((setValue - formatLv - 10) * 100) / formatLv) -
-          selectedPokemon.stats[statsIndex] * 2 -
-          formatIndividualValue) *
-        4
-    }
-    // HP以外の計算では、性格補正を修正してから努力値の逆算を行う必要がある
-    if (statsIndex !== HP_INDEX) {
-      const effortValue = numberToInt(stats[statsIndex].effortValue)
-      const selectedNatureStat = selectedNature.stats[statsIndex]
-      if (setValue % 11 === 10 && selectedNatureStat === UPPER_NATURE) {
-        if (
-          setValue >=
-          Math.floor(
-            (Math.floor(
-              ((selectedPokemon.stats[statsIndex] * 2 +
-                formatIndividualValue +
-                Math.floor(effortValue / 4)) *
-                formatLv) /
-                100
-            ) +
-              5) *
-              selectedNatureStat
-          )
-        ) {
-          setValue += 1
-        } else {
-          setValue -= 1
-        }
-      }
-      if (selectedNatureStat === UPPER_NATURE) {
-        setValue = Math.ceil(setValue / UPPER_NATURE)
-      } else if (selectedNatureStat === LOWER_NATURE) {
-        setValue = Math.ceil(setValue / LOWER_NATURE)
-      }
-      setValue =
-        (Math.ceil(((setValue - 5) * 100) / formatLv) -
-          selectedPokemon.stats[statsIndex] * 2 -
-          formatIndividualValue) *
-        4
-    }
+  //   // HPのみ計算式が異なる
+  //   if (statsIndex === HP_INDEX) {
+  //     setValue =
+  //       (Math.ceil(((setValue - formatLv - 10) * 100) / formatLv) -
+  //         selectedPokemon.stats[statsIndex] * 2 -
+  //         formatIndividualValue) *
+  //       4
+  //   }
+  //   // HP以外の計算では、性格補正を修正してから努力値の逆算を行う必要がある
+  //   if (statsIndex !== HP_INDEX) {
+  //     const effortValue = numberToInt(stats[statsIndex].effortValue)
+  //     const selectedNatureStat = selectedNature.stats[statsIndex]
+  //     if (setValue % 11 === 10 && selectedNatureStat === UPPER_NATURE) {
+  //       if (
+  //         setValue >=
+  //         Math.floor(
+  //           (Math.floor(
+  //             ((selectedPokemon.stats[statsIndex] * 2 +
+  //               formatIndividualValue +
+  //               Math.floor(effortValue / 4)) *
+  //               formatLv) /
+  //               100
+  //           ) +
+  //             5) *
+  //             selectedNatureStat
+  //         )
+  //       ) {
+  //         setValue += 1
+  //       } else {
+  //         setValue -= 1
+  //       }
+  //     }
+  //     if (selectedNatureStat === UPPER_NATURE) {
+  //       setValue = Math.ceil(setValue / UPPER_NATURE)
+  //     } else if (selectedNatureStat === LOWER_NATURE) {
+  //       setValue = Math.ceil(setValue / LOWER_NATURE)
+  //     }
+  //     setValue =
+  //       (Math.ceil(((setValue - 5) * 100) / formatLv) -
+  //         selectedPokemon.stats[statsIndex] * 2 -
+  //         formatIndividualValue) *
+  //       4
+  //   }
 
-    // 【共通の処理】計算した値を代入する
-    const verifiedSetValue = valueVerification(setValue, MAX_EV)
-    updateEffortValue(verifiedSetValue, statsIndex)
-  }
+  //   // 【共通の処理】計算した値を代入する
+  //   const verifiedSetValue = valueVerification(setValue, MAX_EV)
+  //   updateEffortValue(verifiedSetValue, statsIndex)
+  // }
 
   return (
     <Grid item xs={5} sx={{ pl: { xs: 2, sm: 3 }, display: 'flex' }}>
@@ -164,7 +169,7 @@ export const RealNumberField = (props: Props) => {
         label={stats[statsIndex].name}
         value={stats[statsIndex].realNumber}
         inputRef={realNumberRef}
-        onBlur={onBlur}
+        onChange={onChange}
         onClick={onSelected}
         variant="standard"
         InputLabelProps={{
