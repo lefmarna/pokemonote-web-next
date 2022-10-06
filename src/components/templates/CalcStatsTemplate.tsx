1ea09e14695442ddc219936755442da45aca325b
lefmarna <panume5ya@gmail.com>
2022-10-06 23:51:07 +0900

    fix: 実数値の変更に努力値がリンクするよう実装

--- src/components/templates/CalcStatsTemplate.tsx
+++ src/components/templates/CalcStatsTemplate.tsx
@@ -4,11 +4,16 @@ import {
   ATTACK_INDEX,
   DEFENCE_INDEX,
   HP_INDEX,
+  LOWER_NATURE,
+  MAX_EV,
+  MAX_REAL_NUMBER,
+  MIN_LEVEL,
   SPEED_INDEX,
   SP_ATTACK_INDEX,
   SP_DEFENCE_INDEX,
+  UPPER_NATURE,
 } from '../../utils/constants'
-import { numberToInt } from '../../utils/utilities'
+import { convertToInteger, numberToInt, valueVerification } from '../../utils/utilities'
 import { BaseStatsField } from '../organisms/BaseStatsField'
 import { EffortValueField } from '../organisms/EffortValueField'
 import { IndividualValueField } from '../organisms/IndividualValueField'
@@ -109,9 +114,64 @@ export const CalcStatsTemplate = (props: Props) => {
   }
 
   const updateRealNumber = (realNumber: number | '', statsIndex: number) => {
+    let setValue = Number(convertToInteger(realNumber, MAX_REAL_NUMBER, false))
+    const formatLv = numberToInt(level, MIN_LEVEL)
+    const formatIndividualValue = numberToInt(stats[statsIndex].individualValue)
+
+    // HPのみ計算式が異なる
+    if (statsIndex === HP_INDEX) {
+      setValue =
+        (Math.ceil(((setValue - formatLv - 10) * 100) / formatLv) -
+          selectedPokemon.stats[statsIndex] * 2 -
+          formatIndividualValue) *
+        4
+    }
+    // HP以外の計算では、性格補正を修正してから努力値の逆算を行う必要がある
+    if (statsIndex !== HP_INDEX) {
+      const effortValue = numberToInt(stats[statsIndex].effortValue)
+      const selectedNatureStat = selectedNature.stats[statsIndex]
+      if (setValue % 11 === 10 && selectedNatureStat === UPPER_NATURE) {
+        if (
+          setValue >=
+          Math.floor(
+            (Math.floor(
+              ((selectedPokemon.stats[statsIndex] * 2 +
+                formatIndividualValue +
+                Math.floor(effortValue / 4)) *
+                formatLv) /
+                100
+            ) +
+              5) *
+              selectedNatureStat
+          )
+        ) {
+          setValue += 1
+        } else {
+          setValue -= 1
+        }
+      }
+      if (selectedNatureStat === UPPER_NATURE) {
+        setValue = Math.ceil(setValue / UPPER_NATURE)
+      } else if (selectedNatureStat === LOWER_NATURE) {
+        setValue = Math.ceil(setValue / LOWER_NATURE)
+      }
+      setValue =
+        (Math.ceil(((setValue - 5) * 100) / formatLv) -
+          selectedPokemon.stats[statsIndex] * 2 -
+          formatIndividualValue) *
+        4
+    }
+
+    // 【共通の処理】計算した値を代入する
+    const effortValue = valueVerification(setValue, MAX_EV)
+
     const newStats = stats.map((stat, index) => {
       if (index === statsIndex) {
-        return { ...stat, realNumber }
+        return {
+          ...stat,
+          realNumber,
+          effortValue,
+        }
       }
       return stat
     })
