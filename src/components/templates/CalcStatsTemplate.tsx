9e434d3206539680f34e46a29b5a74573f5a2e49
lefmarna <panume5ya@gmail.com>
2022-11-11 22:50:17 +0900

    feat: パフォーマンス最適化

--- src/components/templates/CalcStatsTemplate.tsx
+++ src/components/templates/CalcStatsTemplate.tsx
@@ -1,5 +1,5 @@
 import { Container, Grid } from '@mui/material'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { Nature, PokemonData, Stat } from '../../types'
 import {
   ATTACK_INDEX,
@@ -45,55 +45,62 @@ export const CalcStatsTemplate = (props: Props) => {
     updateStats,
   } = props
 
-  const getStat = (index: number, tmpEv = 0): number => {
-    const formatLv = numberToInt(Number(level), 1)
-    const formatIndividualValue = numberToInt(stats[index].individualValue)
-    let formatEffortValue = 0
-    // 耐久調整ボタンから呼び出した場合は、仮の努力値を代入する
-    if (tmpEv) {
-      formatEffortValue = tmpEv
-    } else {
-      formatEffortValue = numberToInt(props.stats[index].effortValue)
-    }
-    if (index === HP_INDEX) {
-      if (selectedPokemon.name === 'ヌケニン') return 1
-      return (
-        Math.floor(
-          ((selectedPokemon.stats[index] * 2 +
-            formatIndividualValue +
-            Math.floor(formatEffortValue / 4)) *
-            formatLv) /
-            100
-        ) +
-        10 +
-        formatLv
-      )
-    } else {
-      return Math.floor(
-        (Math.floor(
-          ((selectedPokemon.stats[index] * 2 +
-            formatIndividualValue +
-            Math.floor(formatEffortValue / 4)) *
-            formatLv) /
-            100
-        ) +
-          5) *
-          selectedNature.stats[index]
-      )
-    }
-  }
+  const getStat = useMemo(
+    () =>
+      (index: number, tmpEv = 0): number => {
+        const formatLv = numberToInt(Number(level), 1)
+        const formatIndividualValue = numberToInt(stats[index].individualValue)
+        let formatEffortValue = 0
+        // 耐久調整ボタンから呼び出した場合は、仮の努力値を代入する
+        if (tmpEv) {
+          formatEffortValue = tmpEv
+        } else {
+          formatEffortValue = numberToInt(props.stats[index].effortValue)
+        }
+        if (index === HP_INDEX) {
+          if (selectedPokemon.name === 'ヌケニン') return 1
+          return (
+            Math.floor(
+              ((selectedPokemon.stats[index] * 2 +
+                formatIndividualValue +
+                Math.floor(formatEffortValue / 4)) *
+                formatLv) /
+                100
+            ) +
+            10 +
+            formatLv
+          )
+        } else {
+          return Math.floor(
+            (Math.floor(
+              ((selectedPokemon.stats[index] * 2 +
+                formatIndividualValue +
+                Math.floor(formatEffortValue / 4)) *
+                formatLv) /
+                100
+            ) +
+              5) *
+              selectedNature.stats[index]
+          )
+        }
+      },
+    [level, props.stats, selectedNature.stats, selectedPokemon.name, selectedPokemon.stats, stats]
+  )
 
   /**
    * 実数値は努力値の更新による自動計算によって求めるため、直接代入してはいけない。
    */
-  const realNumbers = [
-    getStat(HP_INDEX),
-    getStat(ATTACK_INDEX),
-    getStat(DEFENCE_INDEX),
-    getStat(SP_ATTACK_INDEX),
-    getStat(SP_DEFENCE_INDEX),
-    getStat(SPEED_INDEX),
-  ]
+  const realNumbers = useMemo(
+    () => [
+      getStat(HP_INDEX),
+      getStat(ATTACK_INDEX),
+      getStat(DEFENCE_INDEX),
+      getStat(SP_ATTACK_INDEX),
+      getStat(SP_DEFENCE_INDEX),
+      getStat(SPEED_INDEX),
+    ],
+    [getStat]
+  )
 
   /**
    * 個体値を更新する
