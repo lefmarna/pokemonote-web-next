82fcd893b3308500f1910cc691c06f858744ed6b
lefmarna <panume5ya@gmail.com>
2022-11-16 23:21:44 +0900

    feat: テキストエリアの準備

--- src/components/organisms/CalcStatsOptions.tsx
+++ src/components/organisms/CalcStatsOptions.tsx
@@ -14,6 +14,7 @@ import {
   RadioGroup,
   Select,
   SelectChangeEvent,
+  TextField,
 } from '@mui/material'
 import { ChangeEvent, useState } from 'react'
 import { Stat } from '../../types'
@@ -160,6 +161,9 @@ export const CalcStatsOptions = (props: Props) => {
             </Button>
           </Box>
         </Grid>
+      </Grid>
+      <Grid container>
+        <TextField value={description} multiline />
       </Grid>
     </Container>
   )
