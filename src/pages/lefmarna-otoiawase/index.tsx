b13f3fb95c67a11945932c869b48cc83e8b24cb9
lefmarna <panume5ya@gmail.com>
2023-03-19 10:44:47 +0900

    fix: お問い合わせページを暫定で作成

---
+++ src/pages/lefmarna-otoiawase/index.tsx
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import {
+  TextField,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Container,
+} from '@mui/material'
+import { AccountCircle, Email } from '@mui/icons-material'
+
+export default function Contact() {
+  const router = useRouter()
+
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsLoading(true)
+    try {
+      await fetch('/lefmarna-otoiawase', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, message }),
+      })
+      router.push('/lefmarna-otoiawase/thanks')
+    } catch (error) {
+      router.push('/')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>お問い合わせ</title>
+      </Head>
+      <Container>
+        <form onSubmit={handleSubmit}>
+          <Card sx={{ maxWidth: 540, mx: 'auto', mt: 5 }}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div" align="center">
+                Pokemonote - お問い合わせ
+              </Typography>
+              <TextField
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                type="text"
+                name="name"
+                id="name"
+                label="お名前"
+                fullWidth
+                sx={{ mt: 3 }}
+                InputProps={{
+                  startAdornment: <AccountCircle />,
+                }}
+              />
+              <TextField
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                type="email"
+                name="email"
+                id="email"
+                label="メールアドレス"
+                fullWidth
+                sx={{ mt: 3 }}
+                InputProps={{
+                  startAdornment: <Email />,
+                }}
+              />
+              <TextField
+                label="お問い合わせ内容"
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                multiline
+                minRows={5}
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                type="submit"
+                variant="contained"
+                color="info"
+                sx={{ mx: 'auto', px: 5 }}
+                disabled={isLoading}
+                size="large"
+              >
+                送信する
+              </Button>
+            </CardActions>
+          </Card>
+        </form>
+      </Container>
+    </>
+  )
+}
