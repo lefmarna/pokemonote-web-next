620f2ea4e3c122b69e028d1dd07e31d39e39a9da
lefmarna <panume5ya@gmail.com>
2023-03-31 21:34:25 +0900

    feat: metaディスクリプションがない際は更新しないように実装

--- src/components/organisms/Meta.tsx
+++ src/components/organisms/Meta.tsx
@@ -6,17 +6,18 @@ type Props = {
 }
 
 export const Meta = (props: Props) => {
-  const {
-    title,
-    metaDescription = 'ポケモンのステータスを計算・管理するためのWebアプリ『Pokemonote』へようこそ！ 素早さ計算機やSVに対応した種族値ランキングといったツールも公開しています。「シンプルで高機能」なツールにこだわって制作していますので、是非お試しください。',
-  } = props
+  const { title, metaDescription } = props
 
   return (
     <Head>
       <title>{title ? `${title} | Pokemonote` : 'Pokemonote'}</title>
-      <meta name="description" content={metaDescription} />
-      <meta name="og:description" content={metaDescription} />
-      <meta name="twitter:description" content={metaDescription} />
+      {metaDescription && (
+        <>
+          <meta name="description" content={metaDescription} />
+          <meta name="og:description" content={metaDescription} />
+          <meta name="twitter:description" content={metaDescription} />
+        </>
+      )}
     </Head>
   )
 }
