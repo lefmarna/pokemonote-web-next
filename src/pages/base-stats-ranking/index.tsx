01416de392abe8cc40d358db85802fa0c370119f
lefmarna <panume5ya@gmail.com>
2023-04-07 23:51:04 +0900

    fix: 不要な記述を削除

--- src/pages/base-stats-ranking/index.tsx
+++ src/pages/base-stats-ranking/index.tsx
@@ -25,11 +25,6 @@ export async function getStaticProps() {
 }
 
 const BaseStatsRanking: NextPage<{ pokemonData: PokemonData[] }> = ({ pokemonData }) => {
-  // updateMeta(
-  //   '種族値ランキング',
-  //   'ポケモンSVの種族値ランキングです。攻撃や特攻、素早さを除いた実質種族値でのリストアップも可能です。伝説や幻のポケモンを表示するオプション、各種ステータスでソートする機能にも対応しています。準伝や600属の暴れる環境で、採用するポケモンに迷った際には、きっとこのツールが役立つことでしょう。'
-  // );
-
   // 【特別なポケモンを表示する】
   const [isShowRanks, setIsShowRanks] = useState({
     legendary: false,
