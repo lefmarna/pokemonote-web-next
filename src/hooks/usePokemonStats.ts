a56e530cf7dc8440743e9dc797f90c33a032675a
lefmarna <panume5ya@gmail.com>
2023-04-17 10:35:12 +0900

    feat: パフォーマンス最適化

--- src/hooks/usePokemonStats.ts
+++ src/hooks/usePokemonStats.ts
@@ -1,30 +1,31 @@
 import { Nature, Pokemon, StatsKey } from '@/types'
 import { LOWER_NATURE, MAX_EV, MAX_REAL_NUMBER, MIN_LEVEL, UPPER_NATURE } from '@/utils/constants'
 import { convertToInteger, numberToInt, valueVerification } from '@/utils/utilities'
+import { useCallback, useMemo } from 'react'
 
 // ポケモンのステータス計算に関するカスタムフック
 export const usePokemonStats = (pokemon: Pokemon) => {
   /**
    * HPの実数値を計算する
    */
-  const calcHpRealNumber = (level: number, baseStat: number, iv: number, ev: number) => {
-    const realNumberCommon = calcRealNumberCommon(level, baseStat, iv, ev)
-    return realNumberCommon + 10 + level
-  }
+  const calcHpRealNumber = useCallback(
+    (level: number, baseStat: number, iv: number, ev: number) => {
+      const realNumberCommon = calcRealNumberCommon(level, baseStat, iv, ev)
+      return realNumberCommon + 10 + level
+    },
+    []
+  )
 
   /**
    * HP以外の実数値を計算する
    */
-  const calcRealNumber = (
-    level: number,
-    baseStat: number,
-    iv: number,
-    ev: number,
-    natureModifier = 1
-  ) => {
-    const realNumberCommon = calcRealNumberCommon(level, baseStat, iv, ev)
-    return Math.floor((realNumberCommon + 5) * natureModifier)
-  }
+  const calcRealNumber = useCallback(
+    (level: number, baseStat: number, iv: number, ev: number, natureModifier = 1) => {
+      const realNumberCommon = calcRealNumberCommon(level, baseStat, iv, ev)
+      return Math.floor((realNumberCommon + 5) * natureModifier)
+    },
+    []
+  )
 
   /**
    * HPとその他の実数値の共通部分の計算ロジック
@@ -40,19 +41,22 @@ export const usePokemonStats = (pokemon: Pokemon) => {
    * 185 -> 186 に更新するとき => 187
    * 187 -> 186 に更新するとき => 185
    */
-  const adjustRealNumber = (
-    newRealNumber: number,
-    level: number,
-    baseStat: number,
-    iv: number,
-    ev: number,
-    natureModifier = 1
-  ) => {
-    if (newRealNumber % 11 !== 10 || natureModifier !== UPPER_NATURE) return newRealNumber
+  const adjustRealNumber = useCallback(
+    (
+      newRealNumber: number,
+      level: number,
+      baseStat: number,
+      iv: number,
+      ev: number,
+      natureModifier = 1
+    ) => {
+      if (newRealNumber % 11 !== 10 || natureModifier !== UPPER_NATURE) return newRealNumber
 
-    const currentRealNumber = calcRealNumber(level, baseStat, iv, ev, natureModifier)
-    return newRealNumber + (newRealNumber >= currentRealNumber ? 1 : -1)
-  }
+      const currentRealNumber = calcRealNumber(level, baseStat, iv, ev, natureModifier)
+      return newRealNumber + (newRealNumber >= currentRealNumber ? 1 : -1)
+    },
+    [calcRealNumber]
+  )
 
   /**
    * 性格補正抜きのピュアな実数値を取得する
@@ -69,49 +73,9 @@ export const usePokemonStats = (pokemon: Pokemon) => {
   }
 
   /**
-   * 実数値から努力値を求める
-   */
-  const getEv = (newRealNumber: number | '', statKey: StatsKey) => {
-    const realNumber = Number(convertToInteger(newRealNumber, MAX_REAL_NUMBER, false))
-    const level = numberToInt(pokemon.level, MIN_LEVEL)
-    const baseStat = pokemon.basicInfo.baseStats[statKey]
-    const iv = numberToInt(pokemon.ivs[statKey])
-    const ev = numberToInt(pokemon.evs[statKey])
-    const natureModifier = statKey !== 'hp' ? getNatureModifier(statKey, pokemon.nature) : 1
-
-    const adjustedRealNumber = adjustRealNumber(realNumber, level, baseStat, iv, ev, natureModifier)
-    const pureRealNumber = getPureRealNumber(adjustedRealNumber, natureModifier)
-
-    const newEv =
-      statKey === 'hp'
-        ? (Math.ceil(((pureRealNumber - level - 10) * 100) / level) - baseStat * 2 - iv) * 4
-        : (Math.ceil(((pureRealNumber - 5) * 100) / level) - baseStat * 2 - iv) * 4
-
-    return valueVerification(newEv, MAX_EV)
-  }
-
-  /**
-   * 実数値を取得する
-   */
-  const getRealNumber = (statKey: StatsKey, tmpEv?: number): number => {
-    const level = numberToInt(Number(pokemon.level), 1)
-    const baseStat = pokemon.basicInfo.baseStats[statKey]
-    const iv = numberToInt(pokemon.ivs[statKey])
-    // 耐久調整ボタンから呼び出した場合は、仮の努力値を代入する
-    const ev = tmpEv ?? numberToInt(pokemon.evs[statKey])
-
-    if (statKey === 'hp') {
-      if (pokemon.basicInfo.name === 'ヌケニン') return 1
-      return calcHpRealNumber(level, baseStat, iv, ev)
-    }
-
-    return calcRealNumber(level, baseStat, iv, ev, getNatureModifier(statKey, pokemon.nature))
-  }
-
-  /**
    * 性格補正値を取得する
    */
-  const getNatureModifier = (stat: StatsKey, nature: Nature) => {
+  const getNatureModifier = useCallback((stat: StatsKey, nature: Nature) => {
     switch (stat) {
       case nature.increasedStat:
         return 1.1
@@ -120,7 +84,78 @@ export const usePokemonStats = (pokemon: Pokemon) => {
       default:
         return 1
     }
-  }
+  }, [])
+
+  /**
+   * 実数値から努力値を求める
+   */
+  const getEv = useCallback(
+    (newRealNumber: number | '', statKey: StatsKey) => {
+      const realNumber = Number(convertToInteger(newRealNumber, MAX_REAL_NUMBER, false))
+      const level = numberToInt(pokemon.level, MIN_LEVEL)
+      const baseStat = pokemon.basicInfo.baseStats[statKey]
+      const iv = numberToInt(pokemon.ivs[statKey])
+      const ev = numberToInt(pokemon.evs[statKey])
+      const natureModifier = statKey !== 'hp' ? getNatureModifier(statKey, pokemon.nature) : 1
+
+      const adjustedRealNumber = adjustRealNumber(
+        realNumber,
+        level,
+        baseStat,
+        iv,
+        ev,
+        natureModifier
+      )
+      const pureRealNumber = getPureRealNumber(adjustedRealNumber, natureModifier)
+
+      const newEv =
+        statKey === 'hp'
+          ? (Math.ceil(((pureRealNumber - level - 10) * 100) / level) - baseStat * 2 - iv) * 4
+          : (Math.ceil(((pureRealNumber - 5) * 100) / level) - baseStat * 2 - iv) * 4
+
+      return valueVerification(newEv, MAX_EV)
+    },
+    [
+      adjustRealNumber,
+      getNatureModifier,
+      pokemon.basicInfo.baseStats,
+      pokemon.evs,
+      pokemon.ivs,
+      pokemon.level,
+      pokemon.nature,
+    ]
+  )
+
+  /**
+   * 実数値を取得する
+   */
+  const getRealNumber = useCallback(
+    (statKey: StatsKey, tmpEv?: number): number => {
+      const level = numberToInt(Number(pokemon.level), 1)
+      const baseStat = pokemon.basicInfo.baseStats[statKey]
+      const iv = numberToInt(pokemon.ivs[statKey])
+      // 耐久調整ボタンから呼び出した場合は、仮の努力値を代入する
+      const ev = tmpEv ?? numberToInt(pokemon.evs[statKey])
+
+      if (statKey === 'hp') {
+        if (pokemon.basicInfo.name === 'ヌケニン') return 1
+        return calcHpRealNumber(level, baseStat, iv, ev)
+      }
+
+      return calcRealNumber(level, baseStat, iv, ev, getNatureModifier(statKey, pokemon.nature))
+    },
+    [
+      pokemon.basicInfo.name,
+      pokemon.nature,
+      pokemon.level,
+      pokemon.basicInfo.baseStats,
+      pokemon.evs,
+      pokemon.ivs,
+      calcHpRealNumber,
+      calcRealNumber,
+      getNatureModifier,
+    ]
+  )
 
   /**
    * HABCDSを取得する
@@ -155,42 +190,44 @@ export const usePokemonStats = (pokemon: Pokemon) => {
   /**
    * 実数値はその他の要素（努力値など）の更新による自動計算によって求める
    */
-  const realNumbers = {
-    hp: getRealNumber('hp'),
-    attack: getRealNumber('attack'),
-    defense: getRealNumber('defense'),
-    spAttack: getRealNumber('spAttack'),
-    spDefense: getRealNumber('spDefense'),
-    speed: getRealNumber('speed'),
-  }
+  const realNumbers = useMemo(() => {
+    return {
+      hp: getRealNumber('hp'),
+      attack: getRealNumber('attack'),
+      defense: getRealNumber('defense'),
+      spAttack: getRealNumber('spAttack'),
+      spDefense: getRealNumber('spDefense'),
+      speed: getRealNumber('speed'),
+    }
+  }, [getRealNumber])
 
   /**
    * 種族値の合計値
    */
-  const totalBaseStats = () => {
+  const totalBaseStats = useCallback(() => {
     return getTotalValue(Object.values(pokemon.basicInfo.baseStats))
-  }
+  }, [pokemon.basicInfo.baseStats])
 
   /**
    * 個体値の合計値
    */
-  const totalIv = () => {
+  const totalIv = useCallback(() => {
     return getTotalValue(Object.values(pokemon.ivs))
-  }
+  }, [pokemon.ivs])
 
   /**
    * 努力値の合計値
    */
-  const totalEv = () => {
+  const totalEv = useCallback(() => {
     return getTotalValue(Object.values(pokemon.evs))
-  }
+  }, [pokemon.evs])
 
   /**
    * 実数値の合計値
    */
-  const totalStats = () => {
+  const totalStats = useCallback(() => {
     return getTotalValue(Object.values(realNumbers))
-  }
+  }, [realNumbers])
 
   return {
     realNumbers,
