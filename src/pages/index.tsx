f2844edd4fda08d1950d313b42736617ec103027
lefmarna <panume5ya@gmail.com>
2023-04-24 23:18:00 +0900

    fix: トップページの不要な画像を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,20 +49,6 @@ export default function Home() {
         <button onClick={routerPushCalcStats}>calc-statsへ</button>
         <button onClick={routerPushPokemons}>pokemonsへ</button>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-          </div>
-        </div>
-
         <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
