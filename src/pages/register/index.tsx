e0de0e4a2017d90d262bb55dd98322b9cda03fb0
lefmarna <panume5ya@gmail.com>
2023-06-18 23:13:25 +0900

    feat: ユーザー登録ページを少しだけ作成

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,3 +1,31 @@
-export default function Register() {
-  return <>登録ページ</>
+import { Meta } from '@/components/organisms/Meta'
+import { noAuthMiddleware } from '@/hocs/noAuthMiddleware'
+import { FormTemplate } from '../../components/templates/FormTemplate'
+import { EmailField } from '../../components/molecules/EmailField'
+import { PasswordField } from '../../components/molecules/PasswordField'
+import { useCallback, useState } from 'react'
+
+const Register = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const updatePassword = useCallback((newPassword: string) => {
+    setPassword(newPassword)
+  }, [])
+
+  return (
+    <>
+      <Meta title="アカウント作成" button-text="新規登録" />
+      <FormTemplate title="アカウント作成">
+        <EmailField required setValue={setEmail} />
+        <PasswordField
+          required
+          value={password}
+          updatePassword={updatePassword}
+        />
+      </FormTemplate>
+    </>
+  )
 }
+
+export default noAuthMiddleware(Register)
