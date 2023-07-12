cc1ed9860c5df1779cc5fdba12815b1b401607a4
lefmarna <panume5ya@gmail.com>
2023-07-12 22:31:03 +0900

    feat: SSRへの路線変更

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = withInterceptStdout(
   {
     reactStrictMode: true,
     swcMinify: true,
-    images: {
-      unoptimized: true, // 静的出力時はtrueにする必要がある
-    },
   },
   (text) => (text.includes('Duplicate atom key') ? '' : text)
 )
