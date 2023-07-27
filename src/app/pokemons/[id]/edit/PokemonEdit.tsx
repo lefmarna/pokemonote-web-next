d691b5aec383ebe957973c4ff5adce3cf4f11d66
lefmarna <panume5ya@gmail.com>
2023-07-28 08:30:19 +0900

    feat: ポケモン編集ページの基盤を作成

--- src/app/pokemons/[id]/edit/PokemonEdit.tsx
+++ src/app/pokemons/[id]/edit/PokemonEdit.tsx
@@ -1,5 +1,176 @@
 'use client'
 
+import { usePathname, useRouter } from 'next/navigation'
+import { useCallback, useState } from 'react'
+import { $axios } from '@/libs/axios'
+import { CalcStatsTemplate } from '@/components/templates/CalcStatsTemplate'
+import type { Nature, NullableStats, Pokemon, PokemonBasicInfo } from '@/types'
+
 export const PokemonEdit = () => {
-  return <div>ポケモン編集</div>
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  const pathname = usePathname()
+
+  const [pokemon, setPokemon] = useState<Pokemon>({
+    basicInfo: {
+      no: 567,
+      name: 'アーケオス',
+      form: '',
+      ranks: [],
+      evolutions: [],
+      baseStats: {
+        hp: 75,
+        attack: 140,
+        defense: 65,
+        spAttack: 112,
+        spDefense: 65,
+        speed: 110,
+      },
+    },
+    level: 50,
+    nature: {
+      name: 'がんばりや',
+      increasedStat: null,
+      decreasedStat: null,
+    },
+    ivs: {
+      hp: 31,
+      attack: 31,
+      defense: 31,
+      spAttack: 31,
+      spDefense: 31,
+      speed: 31,
+    },
+    evs: {
+      hp: '',
+      attack: '',
+      defense: '',
+      spAttack: '',
+      spDefense: '',
+      speed: '',
+    },
+    description: '',
+  })
+
+  /**
+   * ポケモンを更新する
+   */
+  const updatePokemon = useCallback(
+    (pokemon: Pokemon) => {
+      setPokemon(pokemon)
+    },
+    [setPokemon]
+  )
+
+  /**
+   * ポケモンの基本情報を更新する
+   */
+  const updateBasicInfo = useCallback(
+    (newBasicInfo: PokemonBasicInfo) => {
+      setPokemon((prevState) => ({
+        ...prevState,
+        basicInfo: newBasicInfo,
+      }))
+    },
+    [setPokemon]
+  )
+
+  /**
+   * ポケモンの性格を更新する
+   */
+  const updateNature = useCallback(
+    (newNature: Nature) => {
+      setPokemon((prevState) => ({
+        ...prevState,
+        nature: newNature,
+      }))
+    },
+    [setPokemon]
+  )
+
+  /**
+   * ポケモンのレベルを更新する
+   */
+  const updateLevel = useCallback(
+    (newLevel: number | '') => {
+      setPokemon((prevState) => ({
+        ...prevState,
+        level: newLevel,
+      }))
+    },
+    [setPokemon]
+  )
+
+  /**
+   * ポケモンの個体値を更新する
+   */
+  const updateIvs = useCallback(
+    (newIvs: Partial<NullableStats>) => {
+      setPokemon((prevState) => ({
+        ...prevState,
+        ivs: {
+          ...prevState.ivs,
+          ...newIvs,
+        },
+      }))
+    },
+    [setPokemon]
+  )
+
+  /**
+   * ポケモンの努力値を更新する
+   */
+  const updateEvs = useCallback(
+    (newEvs: Partial<NullableStats>) => {
+      setPokemon((prevState) => ({
+        ...prevState,
+        evs: {
+          ...prevState.evs,
+          ...newEvs,
+        },
+      }))
+    },
+    [setPokemon]
+  )
+
+  /**
+   * ポケモンの説明を更新する
+   */
+  const updateDescription = useCallback(
+    (newDescription: string) => {
+      setPokemon((prevState) => ({
+        ...prevState,
+        description: newDescription,
+      }))
+    },
+    [setPokemon]
+  )
+
+  const sendPokemon = async () => {
+    setIsLoading(true)
+    try {
+      await $axios.put(`/pokemons/${pathname}`)
+      router.push(`/pokemons/${pathname}`)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <CalcStatsTemplate
+      title="ポケモン編集"
+      buttonText="更新する"
+      pokemon={pokemon}
+      isLoading={isLoading}
+      updateBasicInfo={updateBasicInfo}
+      updateNature={updateNature}
+      updateLevel={updateLevel}
+      updateIvs={updateIvs}
+      updateEvs={updateEvs}
+      sendPokemon={sendPokemon}
+      updateDescription={updateDescription}
+    />
+  )
 }
