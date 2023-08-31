89e3c094b8a5adbb46c1cb13d90f4e155f171d97
lefmarna <panume5ya@gmail.com>
2023-08-31 23:04:20 +0900

    feat: middlewareからinitializationを除外

--- src/hocs/authMiddleware.tsx
+++ src/hocs/authMiddleware.tsx
@@ -4,14 +4,12 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 import { LoadingPageTemplate } from '@/components/templates/LoadingPageTemplate'
 import { useAuthUserState } from '@/store/authUserState'
-import { useIsInitializationState } from '@/store/isInitializationState'
 import { useRememberRouteMutators } from '@/store/rememberRouteState'
 import type { ComponentType } from 'react'
 
 export const authMiddleware = (WrappedComponent: ComponentType) => {
   return function AuthMiddlewareComponent() {
     const authUser = useAuthUserState()
-    const isInitialization = useIsInitializationState()
     const { updateRememberRoute } = useRememberRouteMutators()
 
     const router = useRouter()
@@ -19,25 +17,15 @@ export const authMiddleware = (WrappedComponent: ComponentType) => {
     const queries = useSearchParams()
     const queryParams = queries.toString() ? `?${queries.toString()}` : ''
 
-    // 初期化が終わっていて、ユーザーがログインしていない場合、ログインページにリダイレクト
+    // ユーザーがログインしていない場合、ログインページにリダイレクト
     useEffect(() => {
-      if (!isInitialization || authUser) return
+      if (authUser) return
 
       updateRememberRoute(`${path}${queryParams}`)
       router.replace('/login')
-    }, [
-      isInitialization,
-      authUser,
-      router,
-      path,
-      queryParams,
-      updateRememberRoute,
-    ])
+    }, [authUser, router, path, queryParams, updateRememberRoute])
 
-    // 初期化が終わっていない（認証状態がまだ確認できない）時に表示される
-    if (!authUser) {
-      return <LoadingPageTemplate />
-    }
+    if (!authUser) return <LoadingPageTemplate />
 
     // ユーザーがログインしている場合は、引数として渡されたコンポーネントを表示
     return <WrappedComponent />
--- src/hocs/noAuthMiddleware.tsx
+++ src/hocs/noAuthMiddleware.tsx
@@ -4,30 +4,24 @@ import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 import { LoadingPageTemplate } from '@/components/templates/LoadingPageTemplate'
 import { useAuthUserState } from '@/store/authUserState'
-import { useIsInitializationState } from '@/store/isInitializationState'
 import { useRememberRouteState } from '@/store/rememberRouteState'
 import type { ComponentType } from 'react'
 
 export const noAuthMiddleware = (WrappedComponent: ComponentType) => {
   return function NoAuthMiddlewareComponent() {
     const authUser = useAuthUserState()
-    const isInitialization = useIsInitializationState()
-
     const rememberRoute = useRememberRouteState()
 
     const router = useRouter()
 
-    // 初期化が終わっていて、ユーザーがログインしている場合、トップページにリダイレクト
+    // ユーザーがログインしている場合、トップページにリダイレクト
     useEffect(() => {
-      if (!isInitialization || !authUser) return
+      if (!authUser) return
 
       router.push(rememberRoute)
-    }, [isInitialization, authUser, router, rememberRoute])
+    }, [authUser, router, rememberRoute])
 
-    // 初期化が終わっていない（認証状態がまだ確認できない）時に表示される
-    if (!isInitialization || authUser) {
-      return <LoadingPageTemplate />
-    }
+    if (authUser) return <LoadingPageTemplate />
 
     // ユーザーがログインしていない場合は、引数として渡されたコンポーネントを表示
     return <WrappedComponent />
