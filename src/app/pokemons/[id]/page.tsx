22615c6ff1cc786a864a1fba71854bbc49041698
lefmarna <panume5ya@gmail.com>
2023-09-08 22:08:32 +0900

    Revert "fix: dynamic = 'error'で検証"
    This reverts commit f3335012cfcb5969de011cab449ff68b322cc60b.

--- src/app/pokemons/[id]/page.tsx
+++ src/app/pokemons/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { PokemonS } from './PokemonS'
 import { getMetadata } from '@/utils/helpers'
 
-export const dynamic = 'error'
+export const dynamic = 'force-static'
 export const dynamicParams = true
+export const generateStaticParams = () => []
 
 export const metadata = getMetadata('ポケモンS')
 
