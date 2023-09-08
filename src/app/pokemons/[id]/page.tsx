e54ddc450163000c3d5934a9d02a9549f28dc735
lefmarna <panume5ya@gmail.com>
2023-09-08 09:06:37 +0900

    fix: generateStaticParamsを追加

--- src/app/pokemons/[id]/page.tsx
+++ src/app/pokemons/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getMetadata } from '@/utils/helpers'
 
 export const dynamic = 'force-static'
 export const dynamicParams = true
+export const generateStaticParams = () => []
 
 export const metadata = getMetadata('ポケモンS')
 
