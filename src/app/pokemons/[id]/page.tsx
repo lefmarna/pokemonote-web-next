f3335012cfcb5969de011cab449ff68b322cc60b
lefmarna <panume5ya@gmail.com>
2023-09-08 09:15:36 +0900

    fix: dynamic = 'error'で検証

--- src/app/pokemons/[id]/page.tsx
+++ src/app/pokemons/[id]/page.tsx
@@ -1,9 +1,8 @@
 import { PokemonS } from './PokemonS'
 import { getMetadata } from '@/utils/helpers'
 
-export const dynamic = 'force-static'
+export const dynamic = 'error'
 export const dynamicParams = true
-export const generateStaticParams = () => []
 
 export const metadata = getMetadata('ポケモンS')
 
