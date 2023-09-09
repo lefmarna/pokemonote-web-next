b16bd0a5e00eda0424bb37a18843a74b6c10d71a
lefmarna <panume5ya@gmail.com>
2023-09-09 09:37:08 +0900

    feat: 素早さ計算機のテーブル表示

--- src/app/calc-speed/CalcSpeed.tsx
+++ src/app/calc-speed/CalcSpeed.tsx
@@ -1,6 +1,15 @@
 'use client'
 
-import { Container, Grid } from '@mui/material'
+import {
+  Container,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material'
+import { useState } from 'react'
 import { Title } from '@/components/molecules/Title'
 import { BaseStatsField } from '@/components/organisms/BaseStatsField'
 import { EffortValueField } from '@/components/organisms/EffortValueField'
@@ -10,10 +19,12 @@ import { StatsTableHeader } from '@/components/organisms/StatsTableHeader'
 import { usePokemonStats } from '@/hooks/usePokemonStats'
 import { usePokemonMutators } from '@/store/pokemonState'
 import { usePokemonState } from '@/store/pokemonState'
+import { RANKS } from '@/utils/constants'
 import type { StatsKey } from '@/types/front'
 
 export const CalcSpeed = () => {
   const pokemon = usePokemonState()
+  const [option, setOption] = useState(false)
 
   const { updateBasicInfo, updateNature, updateLevel, updateIvs, updateEvs } =
     usePokemonMutators()
@@ -28,6 +39,21 @@ export const CalcSpeed = () => {
     const newEv = getEv(newRealNumber, statKey)
 
     updateEvs({ [statKey]: newEv })
+  }
+
+  const filteredRanks = () => {
+    if (option === true) return RANKS
+    return RANKS.filter((rank) => Math.abs(rank.magnification) <= 3)
+  }
+
+  const formatRank = (magnification: number): string => {
+    if (magnification > 0) return `+${magnification}`
+    if (magnification === 0) return `±${magnification}`
+    return String(magnification)
+  }
+
+  const calcBaseSpeed = (percent: number) => {
+    return Math.floor((realNumbers.speed * percent) / 100)
   }
 
   return (
@@ -71,6 +97,29 @@ export const CalcSpeed = () => {
                 updateRealNumber={updateRealNumber}
               />
             </Grid>
+          </Grid>
+          {/* 画面右 */}
+          <Grid item md={9} xs={18} sx={{ mr: { xs: 1, md: 0 } }}>
+            <Table style={{ width: '100%' }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">ランク</TableCell>
+                  <TableCell align="center">素早さ</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredRanks().map((rank) => (
+                  <TableRow key={rank.id}>
+                    <TableCell align="center">
+                      {formatRank(rank.magnification)}
+                    </TableCell>
+                    <TableCell align="center">
+                      {calcBaseSpeed(rank.percent)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </Grid>
         </Grid>
       </Container>
