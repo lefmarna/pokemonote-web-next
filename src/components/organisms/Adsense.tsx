d45ad41c9407ca3b00bea2a49862a6727f8aa121
lefmarna <panume5ya@gmail.com>
2023-10-15 23:24:33 +0900

    Revert "fix: パスの更新時に発火するよう修正"
    This reverts commit d990d7217dd3a5490610617276017117bce463ef.

--- src/components/organisms/Adsense.tsx
+++ src/components/organisms/Adsense.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
 import type { CSSProperties } from 'react'
 
 declare global {
@@ -24,24 +22,24 @@ export const Adsense = (props: Props) => {
     format,
     responsive = 'false',
   } = props
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    if (typeof window === 'undefined') return
-    ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-  }, [pathname, searchParams])
 
   if (process.env.NEXT_PUBLIC_NODE_ENV === 'local') return null
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={style}
-      data-ad-client="ca-pub-3240586325286249"
-      data-ad-slot={slot}
-      data-ad-format={format}
-      data-full-width-responsive={responsive}
-    />
+    <>
+      <ins
+        className="adsbygoogle"
+        style={style}
+        data-ad-client="ca-pub-3240586325286249"
+        data-ad-slot={slot}
+        data-ad-format={format}
+        data-full-width-responsive={responsive}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: '(window.adsbygoogle = window.adsbygoogle || []).push({});',
+        }}
+      />
+    </>
   )
 }
