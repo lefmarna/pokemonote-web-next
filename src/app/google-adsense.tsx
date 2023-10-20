d58089c87f311c02964a64eb70bd2bca0627c751
lefmarna <panume5ya@gmail.com>
2023-10-20 22:43:17 +0900

    feat: scriptタグの最適化

--- src/app/google-adsense.tsx
+++ src/app/google-adsense.tsx
@@ -1,17 +1,14 @@
 'use client'
 
-import Script from 'next/script'
-
 export const GoogleAdsense = () => {
   if (process.env.NEXT_PUBLIC_NODE_ENV === 'local') return null
 
   // NOTE: consoleにAdsense head tag doesn't support data-nscript attribute.の警告が出るため、next/scriptではなく、scriptタグで対応
   return (
-    <Script
+    <script
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3240586325286249"
       crossOrigin="anonymous"
-      strategy="lazyOnload"
     />
   )
 }
