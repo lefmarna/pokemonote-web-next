402494897b3701b53dde5ce7121c05975d2a16cb
lefmarna <panume5ya@gmail.com>
2023-10-23 09:17:47 +0900

    feat: 以前の動作していた形へ

--- src/components/organisms/AdCode.tsx
+++ src/components/organisms/AdCode.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import { memo, useEffect } from 'react'
 import type { CSSProperties } from 'react'
 
@@ -17,15 +16,13 @@ type Props = {
   responsive?: string
 }
 
-export const AdCode = memo(function AdCode(props: Props) {
+export const AdCode = (props: Props) => {
   const {
     slot,
     style = { display: 'block' },
     format = 'auto',
     responsive = 'true',
   } = props
-
-  const pathname = usePathname()
 
   useEffect(() => {
     if (typeof window === 'undefined') return
@@ -36,21 +33,19 @@ export const AdCode = memo(function AdCode(props: Props) {
     } catch (e) {
       console.error(e)
     }
-  }, [pathname])
+  }, [])
 
   return (
-    <div key={pathname}>
-      <ins
-        className="adsbygoogle"
-        style={style}
-        data-ad-client="ca-pub-3240586325286249"
-        data-ad-slot={slot}
-        data-ad-format={format}
-        data-adtest={
-          process.env.NEXT_PUBLIC_NODE_ENV !== 'production' ? 'on' : 'off'
-        }
-        data-full-width-responsive={responsive}
-      />
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={style}
+      data-ad-client="ca-pub-3240586325286249"
+      data-ad-slot={slot}
+      data-ad-format={format}
+      data-adtest={
+        process.env.NEXT_PUBLIC_NODE_ENV !== 'production' ? 'on' : 'off'
+      }
+      data-full-width-responsive={responsive}
+    />
   )
-})
+}
