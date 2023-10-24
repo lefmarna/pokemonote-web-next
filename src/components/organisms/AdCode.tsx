818bb968700ddeaa334a0481639a522235951437
lefmarna <panume5ya@gmail.com>
2023-10-24 23:24:40 +0900

    fix: useEffectを使わない形で

--- src/components/organisms/AdCode.tsx
+++ src/components/organisms/AdCode.tsx
@@ -24,27 +24,34 @@ export const AdCode = memo(function AdCode(props: Props) {
     responsive = 'true',
   } = props
 
-  useLayoutEffect(() => {
-    if (typeof window === 'undefined') return
+  // useLayoutEffect(() => {
+  //   if (typeof window === 'undefined') return
 
-    try {
-      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-    } catch (e) {
-      console.error(e)
-    }
-  }, [])
+  //   try {
+  //     ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // }, [])
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={style}
-      data-ad-client="ca-pub-3240586325286249"
-      data-ad-slot={slot}
-      data-ad-format={format}
-      // data-adtest={
-      //   process.env.NEXT_PUBLIC_NODE_ENV !== 'production' ? 'on' : 'off'
-      // }
-      data-full-width-responsive={responsive}
-    />
+    <>
+      <ins
+        className="adsbygoogle"
+        style={style}
+        data-ad-client="ca-pub-3240586325286249"
+        data-ad-slot={slot}
+        data-ad-format={format}
+        // data-adtest={
+        //   process.env.NEXT_PUBLIC_NODE_ENV !== 'production' ? 'on' : 'off'
+        // }
+        data-full-width-responsive={responsive}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: '(window.adsbygoogle = window.adsbygoogle || []).push({});',
+        }}
+      />
+    </>
   )
 })
