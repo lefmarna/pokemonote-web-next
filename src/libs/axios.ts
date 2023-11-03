3460ca676aa5f6fc427c1f4ac485c2f092496195
lefmarna <panume5ya@gmail.com>
2023-11-03 11:05:09 +0900

    fix: axios1.6.0でも動作するように修正

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -5,4 +5,15 @@ const $axios = axios.create({
   withCredentials: true,
 })
 
+$axios.interceptors.request.use((config) => {
+  const xsrfToken =
+    document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('XSRF-TOKEN='))
+      ?.split('=')[1] ?? ''
+  $axios.defaults.headers['X-XSRF-TOKEN'] = decodeURIComponent(xsrfToken)
+
+  return config
+})
+
 export { $axios, AxiosError, isAxiosError }
