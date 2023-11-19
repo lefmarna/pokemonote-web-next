29971a56ba183fa084908a0c7397f46a65679832
lefmarna <panume5ya@gmail.com>
2023-11-19 22:39:04 +0900

    fix: 幅も修正

--- src/components/organisms/CalcStatsOptions.tsx
+++ src/components/organisms/CalcStatsOptions.tsx
@@ -164,7 +164,7 @@ export const CalcStatsOptions = memo(function CalcStatsOptions(props: Props) {
       <Grid item xs={12} md={6}>
         <AdCode
           slot="1632034496"
-          style={{ display: 'block', height: '232px' }}
+          style={{ display: 'block', height: '160px', width: '215px' }}
         />
       </Grid>
       <Grid item xs={12}>
