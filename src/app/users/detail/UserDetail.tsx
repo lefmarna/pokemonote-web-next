2ce0b892f417aa83709170e480026d960772bf6f
lefmarna <panume5ya@gmail.com>
2024-01-07 20:47:47 +0900

    fix: useEffect等の不要処理を削除

--- src/app/users/detail/UserDetail.tsx
+++ src/app/users/detail/UserDetail.tsx
@@ -1,19 +1,14 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
 import { useSWROpenApi } from '@/libs/swr'
 import { LoadingPageTemplate } from '@/components/templates/LoadingPageTemplate'
 import { PokemonTableTemplate } from '@/components/templates/PokemonTableTemplate'
 import { useAuthUserState } from '@/store/authUserState'
-import type { User, PokemonSummary } from '@/types/openapi/schemas'
 
 export const UserDetail = () => {
   const authUser = useAuthUserState()
   const searchParams = useSearchParams()
-
-  const [user, setUser] = useState<User | null>(null)
-  const [pokemonSummaries, setPokemonSummaries] = useState<PokemonSummary[]>([])
 
   const { data, isLoading } = useSWROpenApi({
     url: '/api/v2/users/{username}',
@@ -26,24 +21,17 @@ export const UserDetail = () => {
     },
   })
 
-  useEffect(() => {
-    if (data === undefined) return
-
-    setUser(data.data.user)
-    setPokemonSummaries(data.data.pokemons.data)
-  }, [data])
-
   if (isLoading) return <LoadingPageTemplate />
 
   const title =
-    authUser?.username === user?.username
+    authUser?.username === data?.data.user.username
       ? 'マイページ'
-      : `${user?.username}さんの投稿`
+      : `${data?.data.user.username}さんの投稿`
 
   return (
     <PokemonTableTemplate
       title={title}
-      pokemons={pokemonSummaries}
+      pokemons={data?.data.pokemons.data}
       paginate={data?.data.pokemons.paginate}
     />
   )
