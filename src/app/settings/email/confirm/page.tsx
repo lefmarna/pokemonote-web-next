432be25bd3ff64f132f1891e643d01df1eb155e2
lefmarna <panume5ya@gmail.com>
2024-03-02 15:44:35 +0900

    feat: メールアドレス確認ページにもsuspense追加

--- src/app/settings/email/confirm/page.tsx
+++ src/app/settings/email/confirm/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from 'react'
 import { SettingsEmailConfirm } from './SettingsEmailConfirm'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('メールアドレスの変更')
 
 export default function Page() {
-  return <SettingsEmailConfirm />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SettingsEmailConfirm />
+    </Suspense>
+  )
 }
