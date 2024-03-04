132ba0142c9a905048f7b6e2854194ead136354d
lefmarna <panume5ya@gmail.com>
2024-03-04 23:09:41 +0900

    feat: ポケモン一覧ページもsuspense追加

--- src/app/pokemons/page.tsx
+++ src/app/pokemons/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from 'react'
 import { PokemonIndex } from './PokemonIndex'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('みんなの投稿')
 
 export default function Page() {
-  return <PokemonIndex />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PokemonIndex />
+    </Suspense>
+  )
 }
