e965fc1e00ea81877256453d0575f817e31b02ba
lefmarna <panume5ya@gmail.com>
2024-03-07 22:20:48 +0900

    feat: パスワード確認ページもsuspense追加

--- src/app/password/confirm/page.tsx
+++ src/app/password/confirm/page.tsx
@@ -1,8 +1,13 @@
+import { Suspense } from 'react'
 import { PasswordConfirm } from './PasswordConfirm'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('パスワード再設定の受付')
 
 export default function Page() {
-  return <PasswordConfirm />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PasswordConfirm />
+    </Suspense>
+  )
 }
