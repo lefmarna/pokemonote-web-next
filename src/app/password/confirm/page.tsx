477d9f50ea2a5be6207f294b1dfa4cb5ff0c6650
lefmarna <panume5ya@gmail.com>
2024-03-12 21:03:45 +0900

    fix: 余分なsuspense削除

--- src/app/password/confirm/page.tsx
+++ src/app/password/confirm/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from 'react'
 import { PasswordConfirm } from './PasswordConfirm'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('パスワード再設定の受付')
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <PasswordConfirm />
-    </Suspense>
-  )
+  return <PasswordConfirm />
 }
