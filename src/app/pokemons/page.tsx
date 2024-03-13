3d7a728839bbab23719e16cfe253e62c412bb73c
lefmarna <panume5ya@gmail.com>
2024-03-13 22:43:22 +0900

    fix: 不必要なsuspense削除

--- src/app/pokemons/page.tsx
+++ src/app/pokemons/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from 'react'
 import { PokemonIndex } from './PokemonIndex'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('みんなの投稿')
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <PokemonIndex />
-    </Suspense>
-  )
+  return <PokemonIndex />
 }
