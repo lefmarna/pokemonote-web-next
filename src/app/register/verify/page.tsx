a966eec28daea823f0e9199aa65a15942233cd99
lefmarna <panume5ya@gmail.com>
2024-03-14 22:41:19 +0900

    fix: 更に不要なSuspense削除

--- src/app/register/verify/page.tsx
+++ src/app/register/verify/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from 'react'
 import { RegisterVerify } from './RegisterVerify'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('メール認証')
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <RegisterVerify />
-    </Suspense>
-  )
+  return <RegisterVerify />
 }
