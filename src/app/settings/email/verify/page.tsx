796fc1154133fc6c0de3eff561b308370059dc3f
lefmarna <panume5ya@gmail.com>
2024-03-15 22:09:56 +0900

    fix: 更に不必要なsuspense削除

--- src/app/settings/email/verify/page.tsx
+++ src/app/settings/email/verify/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from 'react'
 import { SettingsEmailVerify } from './SettingsEmailVerify'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('メールアドレスの更新')
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SettingsEmailVerify />
-    </Suspense>
-  )
+  return <SettingsEmailVerify />
 }
