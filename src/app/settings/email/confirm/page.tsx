277261922b29d61f84db435ba8369658bab441c2
lefmarna <panume5ya@gmail.com>
2024-03-16 17:37:12 +0900

    fix: suspense削除

--- src/app/settings/email/confirm/page.tsx
+++ src/app/settings/email/confirm/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from 'react'
 import { SettingsEmailConfirm } from './SettingsEmailConfirm'
 import { getMetadata } from '@/utils/helpers'
 
 export const metadata = getMetadata('メールアドレスの変更')
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SettingsEmailConfirm />
-    </Suspense>
-  )
+  return <SettingsEmailConfirm />
 }
