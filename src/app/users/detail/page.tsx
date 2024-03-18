bc2906a2f69c139685417b33217e42a7774c26f5
lefmarna <panume5ya@gmail.com>
2024-03-18 21:03:58 +0900

    fix: 最後のsuspense削除

--- src/app/users/detail/page.tsx
+++ src/app/users/detail/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from 'react'
 import { UserDetail } from './UserDetail'
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <UserDetail />
-    </Suspense>
-  )
+  return <UserDetail />
 }
