7bee138eb3a074f3cd98748dbcc1db2f4dd459e3
lefmarna <panume5ya@gmail.com>
2024-04-25 23:34:46 +0900

    fix: 高さを修正

--- src/app/base-stats-ranking/BaseStatsRanking.tsx
+++ src/app/base-stats-ranking/BaseStatsRanking.tsx
@@ -283,6 +283,9 @@ export const BaseStatsRanking = () => {
                   '0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12)',
                 minHeight: '109px',
               },
+              '& .MuiDataGrid-filler': {
+                height: 0,
+              },
               '& .MuiDataGrid-toolbarContainer': {
                 mb: 2,
               },
