22525e20d0ebd14a529c5e54feaa3b1a0278f5b3
lefmarna <panume5ya@gmail.com>
2024-05-19 08:27:56 +0900

    feat: axiosの新設定を適用

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -3,6 +3,7 @@ import axios, { AxiosError, isAxiosError } from 'axios'
 const $axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
   withCredentials: true,
+  withXSRFToken: true,
 })
 
 export { $axios, AxiosError, isAxiosError }
