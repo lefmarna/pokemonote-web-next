cc5ece893519f0da852c42000e84577272965784
lefmarna <panume5ya@gmail.com>
2024-05-26 14:11:01 +0900

    fix: コンポーネントの責務見直し

--- src/components/molecules/LoginAlert.tsx
+++ src/components/molecules/LoginAlert.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled'
 import Alert from '@mui/material/Alert'
-import Container from '@mui/material/Container'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useRememberRouteMutators } from '@/store/rememberRouteState'
 
@@ -22,19 +21,17 @@ export const LoginAlert = () => {
   }
 
   return (
-    <Container maxWidth="xl" disableGutters sx={{ pt: 1.5, px: 1.5 }}>
-      <Alert
-        variant="outlined"
-        severity="info"
-        color="warning"
-        sx={{
-          color: '#ed6c02',
-        }}
-      >
-        計算結果の保存には、
-        <LoginText onClick={goToLoginPage}>ログイン</LoginText>
-        が必要です。
-      </Alert>
-    </Container>
+    <Alert
+      variant="outlined"
+      severity="info"
+      color="warning"
+      sx={{
+        color: '#ed6c02',
+      }}
+    >
+      計算結果の保存には、
+      <LoginText onClick={goToLoginPage}>ログイン</LoginText>
+      が必要です。
+    </Alert>
   )
 }
