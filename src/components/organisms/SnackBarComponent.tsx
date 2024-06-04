f1414b3f084505522016907a28acc3ffc801a5b3
lefmarna <panume5ya@gmail.com>
2024-06-04 22:41:38 +0900

    feat: ひとまずコンポーネントが雑に表示されるように調整

--- src/components/organisms/SnackBarComponent.tsx
+++ src/components/organisms/SnackBarComponent.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { Alert } from '@mui/material'
+import { exit } from 'process'
+import { Alert, Slide } from '@mui/material'
 import Snackbar from '@mui/material/Snackbar'
 import { useSnackbarMutators, useSnackbarState } from '@/store/snackbarState'
 import type { SxProps, Theme } from '@mui/material'
@@ -16,17 +17,21 @@ function SnackbarComponent(props: Props) {
   const { closeSnackbar } = useSnackbarMutators()
 
   return (
-    <Snackbar
-      open={snackbar.isOpen}
-      autoHideDuration={3000}
-      onClose={closeSnackbar}
-      anchorOrigin={snackbar.anchorOrigin}
-      sx={sx}
+    <Slide
+      in={snackbar.isOpen}
+      timeout={{ appear: 100, enter: 200, exit: 500 }}
+      direction="left"
+      // anchorOrigin={snackbar.anchorOrigin}
+      // sx={sx}
     >
-      <Alert severity={snackbar.severity} variant="filled">
+      <Alert
+        severity={snackbar.severity}
+        variant="filled"
+        sx={{ position: 'absolute', zIndex: '2000', width: '100%' }}
+      >
         {snackbar.message}
       </Alert>
-    </Snackbar>
+    </Slide>
   )
 }
 
