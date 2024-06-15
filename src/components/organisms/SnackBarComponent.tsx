df702bedfd7dae5838cb1343454103c08a43031f
lefmarna <panume5ya@gmail.com>
2024-06-15 22:25:32 +0900

    feat: ちょっとだけアニメーションを凝ったものに

--- src/components/organisms/SnackBarComponent.tsx
+++ src/components/organisms/SnackBarComponent.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box } from '@mui/material'
+import { Box, Slide } from '@mui/material'
 import Snackbar from '@mui/material/Snackbar'
 import { useSnackbarMutators, useSnackbarState } from '@/store/snackbarState'
 
@@ -15,6 +15,7 @@ function SnackbarComponent() {
       autoHideDuration={3000}
       onClose={closeSnackbar}
       anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      TransitionComponent={Slide}
     >
       <Box
         bgcolor="success.main"
@@ -23,6 +24,7 @@ function SnackbarComponent() {
           px: 2,
           py: 1,
           borderRadius: '10px',
+          boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
         }}
       >
         {snackbar.message}
